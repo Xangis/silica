ce98cd71d32a0566be086e86037b5f725a463898
Jason Champion <jchampion@zetacentauri.com>
2019-03-11 21:07:22 -0700

    Upgrade from Django 1.7 to Django 1.11.

--- urls.py
+++ urls.py
@@ -1,23 +1,21 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
+import gel.views
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'gel.views.index'),
-    url(r'^packet/(?P<id>\d+)/$', 'gel.views.packet'),
-    url(r'^privacy-policy/$', 'gel.views.privacy'),
-    url(r'^colors/$', 'gel.views.colors'),
-    url(r'^color/(?P<color>[-\w]+)/$', 'gel.views.color'),
-    url(r'^languages/$', 'gel.views.languages'),
-    url(r'^language/(?P<language>[-\w]+)/$', 'gel.views.language'),
-    url(r'^manufacturers/$', 'gel.views.manufacturers'),
-    url(r'^manufacturer/(?P<manufacturer>[-\w]+)/$', 'gel.views.manufacturer'),
-
+urlpatterns = [
+    url(r'^$', gel.views.index),
+    url(r'^packet/(?P<id>\d+)/$', gel.views.packet),
+    url(r'^privacy-policy/$', gel.views.privacy),
+    url(r'^colors/$', gel.views.colors),
+    url(r'^color/(?P<color>[-\w]+)/$', gel.views.color),
+    url(r'^languages/$', gel.views.languages),
+    url(r'^language/(?P<language>[-\w]+)/$', gel.views.language),
+    url(r'^manufacturers/$', gel.views.manufacturers),
+    url(r'^manufacturer/(?P<manufacturer>[-\w]+)/$', gel.views.manufacturer),
     # Uncomment the admin/doc line below to enable admin documentation:
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^adm/', include(admin.site.urls)),
-)
+    # Enable the admin:
+    url(r'^adm/', include(admin.site.urls))
+]
